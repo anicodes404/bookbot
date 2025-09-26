7c9d9988c81cfaebfae72005ef41d7cdf478ddc9
Aishe Ibrahim <anicodes404@gmail.com>
2025-09-25 20:43:18 -0400

    Added report that counts occurences of letters, total count of words.

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-def get_book_text(file_path):
-    with open(file_path, "r") as file: 
-        file_contents = file.read()
-    return file_contents
+import sys
 
+from stats import (
+    get_num_words, 
+    chars_dict_to_sorted_list, 
+    get_chars_dict, 
+)
 
-if __name__ == "__main__":
-    path = "./books/frankenstein.txt"
-    print(get_book_text(path))
+def main(): 
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+def get_book_text(path):
+    with open(path) as f: 
+        return f.read()
+    
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+main()
